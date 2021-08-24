ea9b9ef09b10abec070660011519431b1fab6d41
herdeybayor <shereefdeen4real@gmail.com>
2021-08-24 21:51:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+$("input[type='button']").on("click", ()=>{
+    palindrome(input);
+})
+
+var input;
+
+function palindrome(input) {
+    input = $("input[type='text']").val();
+    var inputLowerCase = input.toLowerCase();
+    var inputArray = inputLowerCase.split("");
+    var reversedArray = inputArray.reverse();
+    var inputString = reversedArray.toString().replaceAll(",", "");
+    var result = inputString.toString();
+
+    if (input === ""){
+        $("p").text("Enter a word!");
+        $("p").css("color", "red");
+    }
+    else if(inputLowerCase === result) {
+        $("p").text("Word is Palindrome");
+        $("p").css("color", "green");
+    } else {
+        $("p").text("Word is not Palindrome");
+        $("p").css("color", "red");
+    };
+}
